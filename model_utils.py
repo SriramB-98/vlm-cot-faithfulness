f88c78fc83f536237521ece3445526a3b4b88ac0
SriramB-98 <bee.sriram1998@gmail.com>
2025-02-20 18:58:15 -0500

    Updated model utils

--- model_utils.py
+++ model_utils.py
@@ -1,8 +1,6 @@
 import torch
 from internvl import internvl_preprocess, get_internvl_model_tokenizer
-from llava_cot import get_llama_model_tokenizer
-from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor, MllamaForConditionalGeneration
-from qwen_vl_utils import process_vision_info
+from transformers import Qwen2VLForConditionalGeneration, AutoProcessor, MllamaForConditionalGeneration
 
 
 def internvl_cvbench_predict(
@@ -59,32 +57,34 @@ def qwen_cvbench_predict(model, processor, sample_to_predict, context_samples, m
     content.extend( [ {"type": "text", "text": "Question:"}, {"type": "image"}, {"type": "text", "text": sample_to_predict["prompt"]}])
 
     messages = [{"role": "user", "content": content}]
-    inputs = processor.apply_chat_template(
-        messages,
-        add_generation_prompt=True,
-        # tokenize=True,
-        return_dict=True,
-        return_tensors="pt"
-    ).to(model.device)
-
+    prompt_text = processor.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True,
+    )
+    inputs = processor(images, text=[prompt_text], padding=True, return_tensors="pt").to(model.device)
+    generated_ids = model.generate(**inputs, max_new_tokens=max_new_tokens, do_sample=do_sample)
+    generated_ids_trimmed = [
+        out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+    ]
+    output = processor.batch_decode(generated_ids_trimmed, skip_special_tokens=True)
+    return output[0]
 
 
 def get_model_tokenizer(model_name, *args, **kwargs):
     if "InternVL" in model_name:
         model, tokenizer = get_internvl_model_tokenizer(model_name, *args, **kwargs)
-        return model, tokenizer
+        return model, tokenizer, internvl_cvbench_predict
     elif "Llama" in model_name:
         model = MllamaForConditionalGeneration.from_pretrained(
             model_name, torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2", device_map="auto"
         )
         processor = AutoProcessor.from_pretrained(model_name)
-        return model, processor
+        return model, processor, llama_cvbench_predict
     elif 'Qwen' in model_name:
         model = Qwen2VLForConditionalGeneration.from_pretrained(
             model_name, torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2", device_map="auto"
         )
         processor = AutoProcessor.from_pretrained(model_name)
-        return model, processor
+        return model, processor, qwen_cvbench_predict
     else:
         raise ValueError(f"Model {model_name} not supported")
     
